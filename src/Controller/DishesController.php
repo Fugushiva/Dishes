13f46274da0fa558a46e0c059fbb18f9df1a101b
=Fugushiva <2111jedelodder@student.epfc.eu>
2023-12-18 11:29:41 +0100

    Possibilité d'ajout de plat dans la dataBase

--- src/Controller/DishesController.php
+++ src/Controller/DishesController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Dishes;
+use App\Form\DishesType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,15 +15,47 @@ class DishesController extends AbstractController
     #[Route('/dishes', name: 'app_dishes')]
     public function index(EntityManagerInterface $em): Response
     {
+
         $repository = $em->getRepository(Dishes::class);
         $entity = $repository->findAll();
 
 
 
 
+
+
+
         return $this->render('dishes/index.html.twig', [
-            "food" => $entity[0],
+            "foods" => $entity,
             'title' => 'Liste des plats dans la DB',
+        ]);
+    }
+
+    #[Route('/dishes/new', name: 'app_dishes_new')]
+    public function new(EntityManagerInterface $em, Request $request): Response
+    {
+        $dishe = new Dishes();
+
+        $form = $this->createForm(DishesType::class, $dishe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dishes = $form->getData();
+
+            $em->persist($dishe);
+
+            $em->flush();
+
+            if ($dishes->getId()) {
+                return $this->redirectToRoute('app_dishes');
+            }
+        }
+
+
+        return $this->render('dishes/new.html.twig', [
+            'title' => "Ajouter un plat",
+            'form' => $form
         ]);
     }
 }
--- src/Form/DishesType.php
+++ src/Form/DishesType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Dishes;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +15,21 @@ class DishesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('regime')
-            ->add('country')
+            ->add('regime', ChoiceType::class, [
+                'choices' => [
+                    'Régimes végétals' => [
+                        "Vegétarien" => "Végétarien",
+                        "Végétalien" => "Végétalien",
+                    ],
+                    "Régime Religieux" => [
+                        "Halal" => "Halal",
+                        "Kasher" => "Kasher",
+                        "Sikhiste" => "Sikhiste",
+                        "Hindouisme" => "Hindou",
+                    ]
+                ]
+            ])
+            ->add('country', CountryType::class)
             ->add('spicy')
             ->add('note')
             ->add('name')
